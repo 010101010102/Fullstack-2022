b10fdfd4e5f168900af3654fc761dc5cc29ae124
badmus-ayomide <107115405+010101010102@users.noreply.github.com>
2023-01-18 11:18:19 +0100

    part 1

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -1,26 +1,60 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+import { useState } from 'react'
+
+const StatisticLine = ({ text, value }) => 
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good+bad+neutral
+
+  if (all===0) {
+    return <>
+      <h3>statistics</h3>
+      <p>no feedback given</p>
+    </>
+  }
+
+  const average = (good - bad ) / all
+  const positive = 100 * (good / all)
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h3>statistics</h3>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={all} />
+          <StatisticLine text='average' value={average} />
+          <StatisticLine text='positive' value={positive + '%'} />
+        </tbody>
+      </table>
+
     </div>
+  )
+}
+
+const Button =  ({ handleClick, label}) => <button onClick={handleClick}>{label}</button>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h3>give feedback</h3>
+      <div>
+        <Button handleClick={() => setGood(good+1) } label='good' />
+        <Button handleClick={() => setNeutral(neutral+1)} label='neutral' />
+        <Button handleClick={() => setBad(bad+1)} label='bad' />
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+
   )
 }
 
